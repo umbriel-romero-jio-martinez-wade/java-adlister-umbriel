a5e840552f972b225bd16c2c9892e82f6567c002
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-07-25 11:00:23 -0500

    adServlet

---
+++ src/main/java/com/codeup/adlister/controllers/IndividualAdServlet.java
@@ -0,0 +1,21 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndividualAdServlet", urlPatterns = "/ad")
+public class IndividualAdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/ads/ad.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
