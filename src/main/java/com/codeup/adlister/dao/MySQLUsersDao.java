91f032da66c3d8eda9c0e46c3c958a3b2b60dcb9
Kelvin <kelvin.daquon.wade@gmail.com>
2022-07-28 16:24:30 -0500

    finsihed kelvin database

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -69,13 +69,15 @@ public class MySQLUsersDao implements Users {
     }
 
     private User extractUser(ResultSet rs) throws SQLException {
-
-        return new User(
-            rs.getLong("id"),
-            rs.getString("username"),
-            rs.getString("email"),
-            rs.getString("password")
-        );
+        if (! rs.next()) {
+            return null;
+        }
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("username"),
+                    rs.getString("email"),
+                    rs.getString("password")
+            );
 
     }
 
